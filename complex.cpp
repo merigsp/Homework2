d70d4b4be42f7a9dcb1d5610b28db7da02357957
merigsp <meri.gasparyan05@yahoo.com>
2024-10-03 19:19:43 +0400

    i'm really tired of fixing

--- complex.cpp
+++ complex.cpp
@@ -1,7 +1,7 @@
+
 #include "complex.h"
 
 Complex::Complex(double r, double i) : real(r), imag(i) {}
-
 
 Complex Complex::operator+(const Complex& other) const {
     return Complex(real + other.real, imag + other.imag);
@@ -10,31 +10,16 @@ Complex Complex::operator+(const Complex& other) const {
 Complex Complex::operator-(const Complex& other) const {
     return Complex(real - other.real, imag - other.imag);
 }
-
-#include "complex.h"
-
-Complex::Complex(double r, double i) : real(r), imag(i) {}
-
-
-Complex Complex::operator+(const Complex& other) const {
-    return Complex(real + other.real, imag + other.imag);
-}
-
-Complex Complex::operator-(const Complex& other) const {
-    return Complex(real - other.real, imag - other.imag);
-}
-
 
 Complex Complex::operator*(double scalar) const {
     return Complex(real * scalar, imag * scalar);
 }
 
-
 double Complex::absolute() const {
     return std::sqrt(real * real + imag * imag);
 }
 
-
 std::ostream& operator<<(std::ostream& os, const Complex& c) {
     os << c.real << " + " << c.imag << "i";
-    r
+    return os;
+}
