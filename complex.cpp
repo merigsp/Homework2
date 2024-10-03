33c05f829873700a3b150c37d3a1b5e1305298e8
merigsp <meri.gasparyan05@yahoo.com>
2024-10-03 18:15:59 +0400

    complex.cpp added

---
+++ complex.cpp
@@ -0,0 +1,40 @@
+#include "complex.h"
+
+Complex::Complex(double r, double i) : real(r), imag(i) {}
+
+
+Complex Complex::operator+(const Complex& other) const {
+    return Complex(real + other.real, imag + other.imag);
+}
+
+Complex Complex::operator-(const Complex& other) const {
+    return Complex(real - other.real, imag - other.imag);
+}
+
+#include "complex.h"
+
+Complex::Complex(double r, double i) : real(r), imag(i) {}
+
+
+Complex Complex::operator+(const Complex& other) const {
+    return Complex(real + other.real, imag + other.imag);
+}
+
+Complex Complex::operator-(const Complex& other) const {
+    return Complex(real - other.real, imag - other.imag);
+}
+
+
+Complex Complex::operator*(double scalar) const {
+    return Complex(real * scalar, imag * scalar);
+}
+
+
+double Complex::absolute() const {
+    return std::sqrt(real * real + imag * imag);
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Complex& c) {
+    os << c.real << " + " << c.imag << "i";
+    r
